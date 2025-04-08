2f660e00cc13bfaeefa787b334d28a05365ef042
Naveen Kumar Singh <ns38390@gmail.com>
2025-04-08 20:25:53 +0530

    refactor webhook handling for user events

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -5,7 +5,7 @@ import User from "../models/User.js"
 
 export const clerkWebhooks = async (req, res)=>{
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET0)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
@@ -21,7 +21,7 @@ export const clerkWebhooks = async (req, res)=>{
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_Url,
+                    imageUrl: data.image_url,
                 }
                 await User.create(userData)
                 res.json({})
@@ -32,7 +32,7 @@ export const clerkWebhooks = async (req, res)=>{
                 const userData = {
                     email: data.email_address[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_Url,
+                    imageUrl: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
