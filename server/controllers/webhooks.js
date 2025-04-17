d620299434499bf537261cfc53a9207d88f35bdc
Naveen Kumar Singh <ns38390@gmail.com>
2025-04-17 21:04:30 +0530

    Replacing case 'payment_intent.succeeded' with case 'checkout.session.completed'

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -72,31 +72,51 @@ export const stripeWebhooks = async (request, response) => {
 
     // Handle the event
     switch (event.type) {
-        case 'payment_intent.succeeded':{
-            const paymentIntent = event.data.object;
-            const paymentIntentId = paymentIntent.id;
+        // case 'payment_intent.succeeded':{
+        //     const paymentIntent = event.data.object;
+        //     const paymentIntentId = paymentIntent.id;
 
-            const session = await stripeInstance.checkout.sessions.list({
-                payment_intent: paymentIntentId
-            })
+        //     const session = await stripeInstance.checkout.sessions.list({
+        //         payment_intent: paymentIntentId
+        //     })
 
-            const { purchaseId } = session.data[0].metadata;
+        //     const { purchaseId } = session.data[0].metadata;
 
-            const purchaseData = await Purchase.findById(purchaseId)
-            const userData = await User.findById(purchaseData.userId)
-            const courseData = await Course.findById(purchaseData.courseId.toString())
+        //     const purchaseData = await Purchase.findById(purchaseId)
+        //     const userData = await User.findById(purchaseData.userId)
+        //     const courseData = await Course.findById(purchaseData.courseId.toString())
 
-            courseData.enrolledStudents.push(userData)
-            await courseData.save()
+        //     courseData.enrolledStudents.push(userData)
+        //     await courseData.save()
 
-            userData.enrolledCourses.push(courseData._id)
-            await userData.save()
+        //     userData.enrolledCourses.push(courseData._id)
+        //     await userData.save()
 
-            purchaseData.status = 'completed'
-            await purchaseData.save()
+        //     purchaseData.status = 'completed'
+        //     await purchaseData.save()
 
+        //     break;
+        // }
+
+        case 'checkout.session.completed': {
+            const session = event.data.object;
+        
+            const purchaseId = session.metadata.purchaseId;
+            const purchaseData = await Purchase.findById(purchaseId);
+            const userData = await User.findById(purchaseData.userId);
+            const courseData = await Course.findById(purchaseData.courseId.toString());
+        
+            courseData.enrolledStudents.push(userData);
+            await courseData.save();
+        
+            userData.enrolledCourses.push(courseData._id);
+            await userData.save();
+        
+            purchaseData.status = 'completed';
+            await purchaseData.save();
+        
             break;
-        }
+        }        
 
         case 'payment_intent.payment_failed':{
             const paymentIntent = event.data.object;
