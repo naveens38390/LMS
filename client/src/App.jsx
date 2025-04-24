d26f1c1e0d772c558b39765a03de22a0e1677bb4
Naveen Kumar Singh <ns38390@gmail.com>
2025-04-24 22:01:22 +0530

    key update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import{ Route, Routes } from 'react-router-dom'
 import { useMatch } from 'react-router-dom'
 import Home from './pages/student/Home'
